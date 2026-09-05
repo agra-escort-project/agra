10741309b0b9bd563dc1e70fa84c829057e627ff
agra-escort-project <devrajsingh914933@gmail.com>
2026-09-05 23:28:57 +0530

    add

--- src/components/ModelCard.tsx
+++ src/components/ModelCard.tsx
@@ -35,7 +35,7 @@ export default function ModelCard({ model, imageHeight = '350px', showButtons = 
                     <div className="flex items-center mb-[1.2rem]">
             <h3 className="text-[1.6rem] m-0 text-[var(--text-primary)] font-extrabold flex items-center">
               {model.name}
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="inline-block ml-1 -mt-0.5 w-[22px] h-[22px] text-[#10B981]" title="Verified Profile">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="inline-block ml-1 -mt-0.5 w-[22px] h-[22px] text-[#10B981]">
                 <path fillRule="evenodd" d="M8.603 3.799A4.49 4.49 0 0 1 12 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 0 1 3.498 1.307 4.491 4.491 0 0 1 1.307 3.497A4.49 4.49 0 0 1 21.75 12a4.49 4.49 0 0 1-1.549 3.397 4.491 4.491 0 0 1-1.307 3.497 4.491 4.491 0 0 1-3.497 1.307A4.49 4.49 0 0 1 12 21.75a4.49 4.49 0 0 1-3.397-1.549 4.49 4.49 0 0 1-3.498-1.306 4.491 4.491 0 0 1-1.307-3.498A4.49 4.49 0 0 1 2.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 0 1 1.307-3.497 4.49 4.49 0 0 1 3.497-1.307Zm7.007 6.387a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clipRule="evenodd" />
               </svg>
             </h3>
